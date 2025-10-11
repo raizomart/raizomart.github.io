d36e10be8fe8372866ed1b05a59097556fc2cc13
raizomart <adarmanthony@gmail.com>
2025-10-11 00:35:30 -0700

    feat: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/raizomart.github.io/", // Must be included for project sites
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
